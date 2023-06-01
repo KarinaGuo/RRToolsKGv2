87c87718b70fc98027fd8f0fd42052dcecbdccce
KarinaGuo <100897883+KarinaGuo@users.noreply.github.com>
2023-06-01 11:04:47 +1000

    read.table -> read.csv

--- R/cluster_allele_seqs_reference.r
+++ R/cluster_allele_seqs_reference.r
@@ -48,8 +48,8 @@ cluster_allele_seqs_reference <- function(basedir, species, dataset, cutoff, cdb
    cat("   Clusting complete... Matching to locus list file... \n")
    locus_list <- paste(fa_dir,"/",species,"_",dataset,"_locus_list.csv",sep="")
 
-   lt <- read.table(locus_list, header=FALSE, sep=",")
-   tc <- read.table(cluster_tnam, header=FALSE, sep=",")
+   lt <- read.csv(locus_list, header=FALSE, sep=",")
+   tc <- read.csv(cluster_tnam, header=FALSE, sep=",")
 
    i_lt_tc <- match(lt[,2], tc[,1])
 
